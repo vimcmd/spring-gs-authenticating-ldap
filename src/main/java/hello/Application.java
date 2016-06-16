f70903a9ec83327ff1911fba5b299aa4053bc137
vim.cmd <vim.cmd@gmail.com>
2016-06-16 13:41:48 +0500

    added note about @EnableWebMvc

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,6 +3,11 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Note: Normally you would add @EnableWebMvc for a Spring MVC app, but Spring Boot adds it automatically when it
+ * sees spring-webmvc on the classpath. This flags the application as a web application and activates key
+ * behaviors such as setting up a DispatcherServlet.
+ */
 @SpringBootApplication
 public class Application {
 
